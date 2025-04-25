6849db2e1147e75fd17619fd3bdca40571e9ac9e
fractalMinder <graveofwhale@gmail.com>
2025-04-25 18:06:21 +0900

    구현중중

--- App.js
+++ App.js
@@ -1,21 +1,106 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useRef, useState } from "react";
+import { Animated, PanResponder, Text, View } from "react-native";
+import styled from "styled-components/native";
+import { Ionicons } from "@expo/vector-icons";
+
+
+const BLACK_COLOR = "#1e272e";
+const GREY = "#485460";
+const GREEN = "#2ecc71";
+const RED = "#e74c3c";
+
+const Container = styled.View`
+  flex: 1;
+  background-color: ${BLACK_COLOR};
+`;
+const Edge = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+const WordContainer = styled(Animated.createAnimatedComponent(View))`
+  width: 100px;
+  height: 100px;
+  justify-content: center;
+  align-items: center;
+  background-color: ${GREY};
+  border-radius: 50px;
+`;
+const Word = styled.Text`
+  font-size: 38px;
+  font-weight: 500;
+  color: ${(props) => props.color};
+`;
+const Center = styled.View`
+  flex: 3;
+  justify-content: center;
+  align-items: center;
+`;
+const IconCard = styled(Animated.createAnimatedComponent(View))`
+  background-color: white;
+  padding: 5px 10px;
+  border-radius: 10px;
+`;
+
 
 export default function App() {
+  // Values
+  const scale = useRef(new Animated.Value(1)).current
+  const position = useRef(new Animated.ValueXY({ x: 0, y: 0 })).current
+  // Animations
+  const onPressIn = Animated.spring(scale, {
+    toValue: 0.9,
+    useNativeDriver: true,
+  })
+  const onPressOut = Animated.spring(scale, {
+    toValue: 1,
+    useNativeDriver: true,
+  })
+
+  // Pan Responders
+  const panResponder = useRef(PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderMove: (_, { dx, dy }) => {
+
+    },
+    onPanResponderGrant: () => {
+      onPressIn.start()
+    },
+    onPanResponderRelease: () => {
+      onPressOut.start()
+    },
+
+
+  })).current
+  // States
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Container>
+      <Edge>
+        <WordContainer>
+          <Word color={GREEN}>알아</Word>
+        </WordContainer>
+      </Edge>
+      <Center>
+        <IconCard
+          {...panResponder.panHandlers}
+          style={{
+            transform: [
+              ...position.getTranslateTransform(),
+              { scale }
+            ],
+          }}>
+          <Ionicons name="beer" color={GREY} size={66} />
+        </IconCard>
+      </Center>
+      <Edge>
+        <WordContainer>
+          <Word color={RED}>몰라</Word>
+        </WordContainer>
+      </Edge>
+    </Container>
+
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
